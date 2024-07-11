1e2822fbe1e7ea8a7c067ee64d4da7e493a2c9ac
SlimWoody <125416780+SlimWoody@users.noreply.github.com>
2024-07-11 13:24:58 +0400

    Added HomeWork 1

---
+++ homeWork_1.js
@@ -0,0 +1,16 @@
+// HomeWork 1 JS
+// Task 1
+
+const degreeCelsius = 36;
+let degreeFahrenheit = (9 / 5) * degreeCelsius + 32;
+
+console.log(`${degreeCelsius} градусов по Цельсию = ${degreeFahrenheit} градусов по Фарингейту`);
+
+// Task 2 
+
+const myName = 'Pavel';
+let adminName = myName;
+
+console.log(`Имя администратора ${adminName}`);
+
+
